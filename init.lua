2f61e77191a745efcb2800e554a441f9bcdd8857
EchoTheDeveloepr <admin@echothedeveloper.com>
2025-01-11 01:07:00 +0000

    Organised options/settings into lua/settings.lua * organised * spent time on organising * why am i doing this * why cant i stick to sublime

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set noexpandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -17,6 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("settings")
 require("lazy").setup("plugins")
 
 
